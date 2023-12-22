dcb90aa85ab52e583e4f9cd3cf690bf8d0d0c85f
Mateusz <mateuszkrolik87@gmail.com>
2023-12-22 22:20:47 +0100

    👺 4th Commit
    Created ExpensesOutput component to display a summary and a list of expenses.
    lit the component into smaller sub-components: ExpensesSummary and ExpensesList for better structure and readability.
    The ExpensesOutput component now imports and uses these sub-components.

--- App.js
+++ App.js
@@ -50,7 +50,7 @@ function ExpensesOverview() {
 export default function App() {
   return (
     <>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen
