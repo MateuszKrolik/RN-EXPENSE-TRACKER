b5615d65d7ab4cf9b736a24f6b684d532da9c21d
Mateusz <mateuszkrolik87@gmail.com>
2023-12-23 22:57:05 +0100

    22nd Commit
    Created a custom Input component for the expense form.
    This component includes a label and a TextInput field.
    The label and TextInput configurations are passed as props, allowing for flexible usage of the component across the application.

---
+++ components/ManageExpense/ExpenseForm.js
@@ -0,0 +1,13 @@
+import { TextInput, View } from "react-native";
+
+function ExpenseForm() {
+  return (
+    <View>
+      <TextInput />
+      <TextInput />
+      <TextInput />
+    </View>
+  );
+}
+
+export default ExpenseForm;
