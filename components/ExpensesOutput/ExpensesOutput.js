f1a32a16ed7cbe725a5e0ff400e7dce832d6692c
Mateusz <mateuszkrolik87@gmail.com>
2023-12-23 19:29:24 +0100

    🤑 18th Commit
    Implemented expensesReducer hook in expenses-context.js for managing expenses.
    Added ADD, UPDATE, and DELETE cases in the reducer.
    For ADD, a new expense object is created and added to the existing state array.
    For UPDATE, the index of the expense to be updated is found, and a new object is created with the updated values, which replaces the old object in the state array.
    For DELETE, the state array is filtered to exclude the expense to be deleted.
    Ensured all state updates are done immutably.
    Initialized the state with DUMMY_EXPENSES cut from ExpensesOutput.js file.

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -3,69 +3,6 @@ import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
 import { GlobalStyles } from "../../constants/styles";
 
-const DUMMY_EXPENSES = [
-  {
-    id: "e1",
-    description: "A pair of shoes",
-    amount: 59.99,
-    date: new Date("2023-12-22"),
-  },
-  {
-    id: "e2",
-    description: "A pair of trousers",
-    amount: 89.29,
-    date: new Date("2022-11-22"),
-  },
-  {
-    id: "e3",
-    description: "Some bananas",
-    amount: 5.99,
-    date: new Date("2021-12-01"),
-  },
-  {
-    id: "e4",
-    description: "A book",
-    amount: 14.99,
-    date: new Date("2023-01-11"),
-  },
-  {
-    id: "e5",
-    description: "Another book",
-    amount: 18.59,
-    date: new Date("2022-11-09"),
-  },
-  {
-    id: "e6",
-    description: "Another book",
-    amount: 18.59,
-    date: new Date("2022-11-09"),
-  },
-  {
-    id: "e7",
-    description: "Another book",
-    amount: 18.59,
-    date: new Date("2022-11-09"),
-  },
-  {
-    id: "e8",
-    description: "Another book",
-    amount: 18.59,
-    date: new Date("2022-11-09"),
-  },
-  {
-    id: "e9",
-    description: "Another book",
-    amount: 18.59,
-    date: new Date("2022-11-09"),
-  },
-  {
-    id: "e10",
-    description: "Another book",
-    amount: 18.59,
-    date: new Date("2022-11-09"),
-  },
-];
-
 function ExpensesOutput({ expenses, expensesPeriod }) {
   return (
     <View style={styles.container}>
