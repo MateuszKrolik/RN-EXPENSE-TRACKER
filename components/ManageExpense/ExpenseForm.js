e8ec483b0175c156f7eb591e38d479bb2838e154
Mateusz <mateuszkrolik87@gmail.com>
2023-12-25 17:42:29 +0100

    28th Commit
    Moved form submission logic from ManageExpense to ExpenseForm: Transferred button views and associated styles, implemented submitHandler in ExpenseForm, passed onCancel and submitButtonLabel as props from ManageExpense to ExpenseForm

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,8 +1,9 @@
 import { View, StyleSheet, Text } from "react-native";
 import Input from "./Input";
 import { useState } from "react";
+import Button from "../UI/Button";
 
-function ExpenseForm() {
+function ExpenseForm({ submitButtonLabel, onCancel, isEditing }) {
   const [inputValues, setInputValues] = useState({
     amount: "",
     date: "",
@@ -18,7 +19,7 @@ function ExpenseForm() {
     });
   }
 
-  function ammountChangedHandler() {}
+  function submitHandler() {}
 
   return (
     <View style={styles.form}>
@@ -54,6 +55,14 @@ function ExpenseForm() {
           value: inputValues.description,
         }}
       />
+      <View style={styles.buttons}>
+        <Button style={styles.button} mode="flat" onPress={onCancel}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={submitHandler}>
+          {submitButtonLabel}
+        </Button>
+      </View>
     </View>
   );
 }
@@ -77,5 +86,14 @@ const styles = StyleSheet.create({
   },
   rowInput: {
     flex: 1,
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
   },
 });
